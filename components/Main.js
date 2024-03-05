c837f9a1130f2ba02eaaa220312c2bcb17dd031b
Jordan Powell <jordancolepowell@gmail.com>
2024-03-04 21:31:29 -0600

    run serve showing a blank screen when executed

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,6 @@
 import html from "html-literal";
 import * as views from "./views";
 
-export default () => html`
-  ${views["Bio"]()} ${views["Home"]()} ${views["Order"]()} ${views["Pizza"]()}
+export default state => html`
+  ${views[state.view](state)}
 `;
